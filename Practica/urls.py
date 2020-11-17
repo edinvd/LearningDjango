3cc5d0f561d59363cb30b5f424f114d3652adee6
Edin <edinvasquez412@gmail.com>
2020-11-16 22:19:38 -0500

    Autenticación - Desarrolle el algoritmo que me permita que un usuario ingrese correctamente con su username y su password, de lo contrario este devolveria Usuario no autenticado

--- Practica/urls.py
+++ Practica/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('usuarios/login', views.login, name='login'),
+    path('usuarios/login', views.login_view, name='login'),
     path('admin/', admin.site.urls),
 ]
--- Practica/views.py
+++ Practica/views.py
@@ -1,28 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.contrib.auth import login
+from django.contrib.auth import authenticate
+
 def index(request):
     return render(request,'index.html', {
         'message' :  'Listado de productos',
         'title' : 'Productos',
         'products' : [
-            {'title' : 'Playera', 'price' : 15, 'stock' : True}, 
-            {'title' : 'Camisa', 'price' : 17, 'stock' : True}, 
-            {'title' : 'Playera', 'price' : 25, 'stock' : True}, 
-            {'title' : 'Casaca', 'price' : 15, 'stock' : False}, 
-            {'title' : 'Blusa', 'price' : 12, 'stock' : True}, 
-            {'title' : 'Zapatillas', 'price' : 20, 'stock' : False}, 
+            {'title' : 'Playera', 'price' : 15, 'stock' : True},
+            {'title' : 'Camisa', 'price' : 17, 'stock' : True},
+            {'title' : 'Playera', 'price' : 25, 'stock' : True},
+            {'title' : 'Casaca', 'price' : 15, 'stock' : False},
+            {'title' : 'Blusa', 'price' : 12, 'stock' : True},
+            {'title' : 'Zapatillas', 'price' : 20, 'stock' : False},
         ]
     })
 
-def login(request):
+def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username') #Diccionario
         password = request.POST.get('password') #None
 
-        print(username)
-        print(password)
-
+        user = authenticate(username=username, password=password) #None
+        if user:
+            login(request, user)
+            print("Usuario autenticado")
+        else:
+            print("Usuario no autenticado")
+            
     return render(request, 'users/login.html', {
 
     })
