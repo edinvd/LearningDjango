f0c3b02060464b0d8a0b8591b61caf38b22ff11e
Iamedinv <iamedinv@gmail.com>
2020-11-23 22:30:59 -0500

    Estilos en formularios - Utilizamos los (attrs) de los formularios de Django para darle estilo a nuestro formulario

--- Practica/forms.py
+++ Practica/forms.py
@@ -2,7 +2,20 @@ from django import forms
 
 class RegisterForm(forms.Form):
     username = forms.CharField(required=True,
-                                min_length=5, max_length=50)
-    email = forms.EmailField(required=True)
-    password = forms.CharField(required=True)
+                                min_length=5, max_length=50,
+                                widget=forms.TextInput(attrs={
+                                    'class': 'form-control',
+                                    'id': 'username',
+                                    'placeholder': 'Username'
+                                }))
+    email = forms.EmailField(required=True,
+                                widget=forms.EmailInput(attrs={
+                                    'class': 'form-control',
+                                    'id': 'email',
+                                    'placeholder': 'name@example.com'
+                                }))
+    password = forms.CharField(required=True,
+                                widget=forms.PasswordInput(attrs={
+                                    'class': 'form-control'
+                                }))
     
